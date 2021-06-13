e382efc34f8274604ce7e515ae5d696e0f7f0f26
Вадим Булат <justliveinlife@gmail.com>
2021-06-13 20:59:58 +0300

    feat: add confirmation modal

---
+++ Pages/Counter.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorProject.Pages
+{
+    public partial class Counter
+    {
+        [Inject] SingletonServices singleton { get; set; }
+        [Inject] TransientServices transient { get; set; }
+
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+
+            singleton.Value = currentCount;
+            transient.Value = currentCount;
+        }
+    }
+}
