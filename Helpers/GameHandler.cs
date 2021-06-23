c51189c158c908030c77b4a07c9494ec8dbfa8ef
Вадим Булат <justliveinlife@gmail.com>
2021-06-23 18:08:20 +0300

    feat: add game with timer and dispose

---
+++ Helpers/GameHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorProject.Helpers
+{
+    public class GameHandler
+    {
+        public RSPOptions Choose { get; set; }
+        public RSPOptions WinCondition { get; set; }
+        public RSPOptions LooseCondition { get; set; }
+        public string Image { get; set; }
+        public GameState GameResult(GameHandler opponent)
+        {
+            if (Choose == opponent.Choose) return GameState.Draw;
+
+            if (LooseCondition == opponent.Choose) return GameState.Loss;
+
+            return GameState.Victory;
+        }
+    }
+}
---
+++ Helpers/GameState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorProject.Helpers
+{
+    public enum GameState
+    {
+        Victory,
+        Loss,
+        Draw
+    }
+}
